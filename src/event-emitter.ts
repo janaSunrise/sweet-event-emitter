29c0b8ad99371a828287e925cd69285d7488d2c0
janaSunrise <warriordefenderz@gmail.com>
2022-09-26 17:44:35 +0530

    Added the event-emitter

---
+++ src/event-emitter.ts
@@ -0,0 +1,46 @@
+export class EventEmitter {
+  public events: Map<string, Set<Function>>;
+
+  constructor() {
+    this.events = new Map();
+  }
+
+  public listeners(event: string) {
+    if (!this.events.has(event)) return [];
+
+    return [...this.events.get(event)!];
+  }
+
+  public on(event: string, listener: Function) {
+    if (!this.events.has(event)) this.events.set(event, new Set());
+
+    this.events.get(event)?.add(listener);
+  }
+
+  public off(event: string, listener: Function) {
+    if (!this.events.has(event)) return;
+
+    this.events.get(event)?.delete(listener);
+  }
+
+  public emit(event: string, ...args: any[]) {
+    if (!this.events.has(event)) return;
+
+    this.events.get(event)?.forEach(listener => listener(...args));
+  }
+
+  public once(event: string, listener: Function) {
+    const wrapper = (...args: any[]) => {
+      listener(...args);
+      this.off(event, wrapper);
+    };
+
+    this.on(event, wrapper);
+  }
+
+  public removeAllListeners(event: string) {
+    if (!this.events.has(event)) return;
+
+    this.events.get(event)?.clear();
+  }
+}
